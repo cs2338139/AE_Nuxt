353adf4bf5c4ce6576a1df54f72255068295b553
cs2338139 <64711409+cs2338139@users.noreply.github.com>
2022-08-18 02:12:56 +0800

    enter

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,21 +2,6 @@ import { defineNuxtConfig } from "nuxt";
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  // head: {
-  //   title:"夢境漫遊：繪本藝術展",
-  //   script: [
-  //     {
-  //       type: "module",
-  //       src: "https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js",
-  //       body: true,
-  //     },
-  //     {
-  //       nomodule: true,
-  //       src: "https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js",
-  //       body: true,
-  //     },
-  //   ],
-  // },
   build: {
     postcss: {
       postcssOptions: {
